a1f07b923d29a09b3958576dbfde712eb26ca5f4
LAB-Cyberware <lab.cyberware@gmail.com>
2025-07-17 18:55:27 -0400

    Quetalahora

--- app/route.js
+++ app/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "mongoose";
-import User from '../../../User';
+import { User } from "User";
 
 export async function GET() {
     connectDB()
