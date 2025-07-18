2db87053fab3db9854428ddc43afd37b2d470662
LAB-Cyberware <lab.cyberware@gmail.com>
2025-07-18 19:42:10 -0400

    LISTADOUSARIOS5

--- app/admin/page.js
+++ app/admin/page.js
@@ -42,8 +42,7 @@ export default function AdminPage() {
       </div>
         
         <div>
-            <button className='mt-6 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded 
-            text-sm mb-4'>
+            <button className='mb-4'>
             Listado de Usuarios
             </button>
         </div>
