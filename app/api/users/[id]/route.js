d78dd01890fafc434ce58e139b80a8aa2276b356
LAB-Cyberware <lab.cyberware@gmail.com>
2025-07-21 18:20:04 -0400

    CAMBIO ROL 13

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
-import { connectDB } from "../../../mongoose"; 
 import User from "../../../User"; 
+import { NextResponse } from 'next/server';
+import { connectDB } from "../../../mongoose";
 
 export async function GET(request, { params }) {
   try {
@@ -22,5 +22,41 @@ export async function GET(request, { params }) {
   } catch (error) {
     console.error("Error fetching specific user:", error);
     return NextResponse.json({ error: 'Error al obtener usuario.' }, { status: 500 });
+  }
+}
+export async function PATCH(request, { params }) {
+  try {
+    await connectDB();
+
+    const { id } = params;
+    const { rol } = await request.json();
+
+    if (!id) {
+      return NextResponse.json({ message: "ID de usuario no proporcionado." }, { status: 400 });
+    }
+
+    if (!rol) {
+      return NextResponse.json({ message: "Rol no proporcionado." }, { status: 400 });
+    }
+
+    // Actualizar el usuario en MongoDB
+    const updatedUser = await User.findByIdAndUpdate(
+      id, 
+      { rol }, 
+      { new: true } // Devuelve el documento actualizado
+    );
+
+    if (!updatedUser) {
+      return NextResponse.json({ message: "Usuario no encontrado." }, { status: 404 });
+    }
+
+    return NextResponse.json({ 
+      message: "Rol actualizado correctamente", 
+      user: updatedUser 
+    });
+
+  } catch (error) {
+    console.error("Error updating user role:", error);
+    return NextResponse.json({ error: 'Error al actualizar rol.' }, { status: 500 });
   }
 }
